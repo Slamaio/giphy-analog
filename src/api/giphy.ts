8b9b02bb75b7523b6b5135375c3d25ac570734b7
Slamaio <konnor.ru92@gmail.com>
2023-05-27 12:48:05 +0300

    feat: giphy api

--- src/api/giphy.ts
+++ src/api/giphy.ts
@@ -2,12 +2,12 @@ import { get } from '@/api/http'
 import { type IGif } from 'env'
 
 const apiKey = import.meta.env.VITE_GIPHY_API_KEY
-const apiBase = 'https://api.giphy.com/v1/gifs/'
+const apiBase = 'https://api.giphy.com/v1/gifs'
 
 export enum EndpointsUrls {
-  trending = apiBase + 'trending',
-  random = apiBase + 'random',
-  search = apiBase + 'search',
+  trending = apiBase + '/trending',
+  random = apiBase + '/random',
+  search = apiBase + '/search',
   id = apiBase,
 }
 
@@ -17,6 +17,46 @@ export default {
       const gif = await get<IGif>(EndpointsUrls.random, {
         api_key: apiKey,
         rating: 'g',
+      })
+      return gif
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async fetchTrendingGifs(limit: number = 25, offset: number = 0) {
+    try {
+      const gif = await get<IGif[]>(EndpointsUrls.trending, {
+        api_key: apiKey,
+        rating: 'g',
+        limit: String(limit),
+        offset: String(offset),
+      })
+      return gif
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async searchGifs(query: string, limit: number = 25, offset: number = 0) {
+    try {
+      const gif = await get<IGif[]>(EndpointsUrls.search, {
+        api_key: apiKey,
+        rating: 'g',
+        q: query,
+        limit: String(limit),
+        offset: String(offset),
+      })
+      return gif
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  async fetchGifById(id: string) {
+    try {
+      const gif = await get<IGif>(`${EndpointsUrls.id}/${id}`, {
+        api_key: apiKey,
       })
       return gif
     } catch (error) {
