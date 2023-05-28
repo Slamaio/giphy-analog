e141ac1874cfbd6ffd678c22b7e54459cb8db59b
Slamaio <konnor.ru92@gmail.com>
2023-05-28 17:02:06 +0300

    feat: layout update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <v-app>
     <AppBar />
-    <RouterView />
+    <v-row no-gutters>
+      <v-col cols="12" md="10" offset-md="1" lg="8" offset-lg="2" xl="6" offset-xl="3">
+        <RouterView />
+      </v-col>
+    </v-row>
     <CopyAlert />
   </v-app>
 </template>
